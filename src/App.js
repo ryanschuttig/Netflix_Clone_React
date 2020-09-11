c53d3188eaeb7600b58dd4e6074fa50f180e8525
ryanschuttig <ryanschuttig@gmail.com>
2020-09-11 14:48:51 -0400

    created main page tabs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './css/App.css';
+import './css/Main.css';
 import { Switch, Route, NavLink } from 'react-router-dom'
 
 import Main from './pages/index'
---
+++ src/components/TabComponent.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import TabDoor from './tabs_nav/TabDoor';
+import TabDevices from './tabs_nav/TabDevices';
+import TabPrices from './tabs_nav/TabPrices';
+import '../css/TabsNav.css';
+
+class TabComponent extends Component {
+    render() {
+        return (
+            <div>
+                <Tabs className="tabs">
+                    <TabList className="tab-nav-container">
+                        <Tab>
+                            <TabDoor />
+                            <p>
+                                <strong>No commitments<br />Cancel online at anytime</strong>
+                            </p>
+                        </Tab>
+                        <Tab>
+                            <TabDevices />
+                            <p style={{ marginTop: '-5.3125rem' }}>
+                                <strong>Watch anywhere</strong>
+                            </p>
+                        </Tab>
+                        <Tab>
+                            <TabPrices />
+                            <p>
+                                <strong>Pick your price</strong>
+                            </p>
+                        </Tab>
+                    </TabList>
+                </Tabs>
+            </div>
+        )
+    }
+}
+
+export default TabComponent;
