c281d3c4b00a3dcfb458f89e1b21acb88c1e9ac8
ryanschuttig <ryanschuttig@gmail.com>
2020-09-19 16:33:18 -0400

    created main button

--- src/components/TabContentOne.js
+++ src/components/TabContentOne.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 
 function TabContentOne() {
     return (
-        <div>
+        <TabContentContainer>
             <div className="container">
                 <div className="tab-content">
                     <span>If you decide Netflix isn't for you - no problem. No commitment. Cancel online anytime.</span>
@@ -13,7 +13,7 @@ function TabContentOne() {
                     <img src={Img} />
                 </div>
             </div>
-        </div>
+        </TabContentContainer>
     )
 }
 
@@ -22,4 +22,17 @@ export default TabContentOne;
 // Main Content Container
 const TabContentContainer = styled.div`
     background: var(--main-deep-dark);
+
+    img {
+        width: 31.875rem;
+    }
+
+    .tab-content {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        grid-gap: 2rem;
+        align-items: center;
+        font-size: 2rem;
+        padding: 2.5rem;
+    }
 `;
