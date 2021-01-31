9fac346441101557347e01e078104f3c7aaabe68
ryanschuttig <ryanschuttig@gmail.com>
2021-01-31 01:50:33 -0500

    styled icons

--- src/components/TabComponent.js
+++ src/components/TabComponent.js
@@ -28,20 +28,22 @@ class TabComponent extends Component {
                             <p className="lgScreen" style={{ marginBottom: '1.875rem' }}>
                                 <strong>No commitments<br />Cancel online at anytime</strong>
                             </p>
-                            <span className="mdScreen">Cancel</span>
+                            <br />
+                            <span className="mdScreen" style={{  marginTop: '0.4rem' }}>Cancel</span>
                         </Tab>
                         <Tab className={`${this.state.tabIndex === 1 ? 'tab-selected active' : null}`}>
                             <TabDevices />
                             <p className="lgScreen" style={{ marginTop: '-5.3125rem' }}>
                                 <strong>Watch anywhere</strong>
                             </p>
-                            <span className="mdScreen">Devices</span>
+                            <span className="mdScreen" style={{  marginTop: '-5.3125rem' }}>Devices</span>
                         </Tab>
                         <Tab className={`${this.state.tabIndex === 2 ? 'tab-selected active' : null}`}>
                             <TabPrices />
                             <p className="lgScreen">
                                 <strong>Pick your price</strong>
                             </p>
+                            <br />
                             <span className="mdScreen">Price</span>
                         </Tab>
                     </TabList>
